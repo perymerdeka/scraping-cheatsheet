953141fc9c4e686186eb82526a2f9b1bd4d11294
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2021-04-29 12:40:24 +0700

    Update urlibparser.py
    add result function

--- urlibparser.py
+++ urlibparser.py
@@ -17,11 +17,16 @@ url_params = {
     'source': 'navbar'
 }
 
-def params_parser(url, params):
+def params_parser(url, params, show_result=False):
     parsed = urlparse(url)
     current_params = dict(parse_qsl(parsed.query))
     params_merger = urlencode({**current_params, **params})
     new_url = parsed._replace(query=params_merger).geturl()
-    return new_url
+    if show_result == False:
+        return
+    else:
+        print(f"parsed URL is: {new_url}")
+    return str(new_url)
+
 
 print(params_parser(base_url, params=url_params))
