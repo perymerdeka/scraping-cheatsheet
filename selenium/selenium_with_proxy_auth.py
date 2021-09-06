fec3e5532595d315872d3f5e1314b9dc1ab1f3f9
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2021-09-06 10:23:33 +0700

    Update selenium_with_proxy_auth.py

--- selenium/selenium_with_proxy_auth.py
+++ selenium/selenium_with_proxy_auth.py
@@ -1,4 +1,12 @@
-def get_driver(use_proxy=False, user_agent: list = None, ip_proxy: str = None, port: str = None, username: str = None,
+import os
+import random
+import zipfile
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_driver(use_proxy=False, user_agent: list = None, ip_proxy: str = None, port: int = None, username: str = None,
                password: str = None, debug=True):
     """Selenium setup"""
     options = Options()
@@ -11,54 +19,54 @@ def get_driver(use_proxy=False, user_agent: list = None, ip_proxy: str = None, p
             pass
 
         manifest_json = """
-                       {
-                           "version": "1.0.0",
-                           "manifest_version": 2,
-                           "name": "Chrome Proxy",
-                           "permissions": [
-                               "proxy",
-                               "tabs",
-                               "unlimitedStorage",
-                               "storage",
-                               "<all_urls>",
-                               "webRequest",
-                               "webRequestBlocking"
-                           ],
-                           "background": {
-                               "scripts": ["background.js"]
-                           },
-                           "minimum_chrome_version":"22.0.0"
-                       }
-                       """
+{
+   "version": "1.0.0",
+   "manifest_version": 2,
+   "name": "Chrome Proxy",
+   "permissions": [
+       "proxy",
+       "tabs",
+       "unlimitedStorage",
+       "storage",
+       "<all_urls>",
+       "webRequest",
+       "webRequestBlocking"
+   ],
+   "background": {
+       "scripts": ["background.js"]
+   },
+   "minimum_chrome_version":"22.0.0"
+}
+"""
 
         background_js = """
-                       var config = {
-                               mode: "fixed_servers",
-                               rules: {
-                               singleProxy: {
-                                   scheme: "http",
-                                   host: "%s",
-                                   port: parseInt(%s)
-                               },
-                               bypassList: ["localhost"]
-                               }
-                           };
-                           chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+var config = {
+       mode: "fixed_servers",
+       rules: {
+       singleProxy: {
+           scheme: "http",
+           host: "%s",
+           port: parseInt(%s)
+       },
+       bypassList: ["localhost"]
+       }
+   };
+   chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
 
-                       function callbackFn(details) {
-                           return {
-                               authCredentials: {
-                                   username: "%s",
-                                   password: "%s"
-                               }
-                           };
-                       }
+function callbackFn(details) {
+   return {
+       authCredentials: {
+           username: "%s",
+           password: "%s"
+       }
+   };
+}
 
-                       chrome.webRequest.onAuthRequired.addListener(
-                                   callbackFn,
-                                   {urls: ["<all_urls>"]},
-                                   ['blocking']
-                       );
+chrome.webRequest.onAuthRequired.addListener(
+           callbackFn,
+           {urls: ["<all_urls>"]},
+           ['blocking']
+);
                        """ % (ip_proxy, port, username, password)
 
         plugin_file = 'temp/plugins/proxy_auth.zip'
@@ -66,7 +74,8 @@ def get_driver(use_proxy=False, user_agent: list = None, ip_proxy: str = None, p
             zp.writestr("manifest.json", manifest_json)
             zp.writestr("background.js", background_js)
 
-        options.extensions(plugin_file)
+        # raise Exception(plugin_file)
+        options.add_extension(plugin_file)
         options.add_argument(argument=f'argument={random.choice(user_agent)}')
         if not debug:
             options.add_argument(argument='--headless')
