589675a6d2ba5cf9cb46234f84acb88bb4d2afef
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2021-09-21 23:35:15 +0700

    Create flask-excel.py

---
+++ flask/flask-excel.py
@@ -0,0 +1,10 @@
+import flask_excel as excel
+
+@app.route('/download', methods=['GET'])
+def download_data():
+    sample_data=[0, 1, 2]
+    excel.init_excel(app)
+    extension_type = "csv"
+    filename = "test123" + "." extension_type
+    d = {'colName': sample_data}
+    return excel.make_response_from_dict(d, file_type=extension_type, file_name=filename)
